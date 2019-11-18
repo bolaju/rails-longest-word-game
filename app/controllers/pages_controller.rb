50e72083d25dc8700d4718b61484c275c661da3b
Justin Boediono <justin.boediono@gmail.com>
2019-11-18 17:01:45 +0800

    longest word game v2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,9 @@ require 'open-uri'
 require 'time'
 
 class PagesController < ApplicationController
+  include PagesHelper
   DATA = { time_start: nil, time_end: nil, word: nil,
-           user_score: nil, exist_message: nil, invalid_message: nil , letters: nil }
+           user_score: nil, exist_message: nil, invalid_message: nil, letters: nil }
   def game
     DATA[:time_start] = Time.now
     @letters = ('A'..'Z').to_a.sample(10).join('')
@@ -18,19 +19,7 @@ class PagesController < ApplicationController
     raw_json = open(url).read
     word_hash = JSON.parse(raw_json)
     DATA[:exist_message] = @exist_message = "Word doesn't exist!" unless word_hash['found']
-    DATA[:user_score] = @score = score
-    DATA[:invalid_message] = @invalid_message = check_word
-  end
-
-  def score
-    time_taken = DATA[:time_end] - DATA[:time_start]
-    DATA[:word].length / time_taken
-  end
-
-  def check_word
-    invalid_letters = DATA[:word].split('').reject do |letter|
-      DATA[:letters].split('').include? letter
-    end
-    return 'Invalid letters used!' unless invalid_letters.length.zero?
+    DATA[:user_score] = @score = score(DATA)
+    DATA[:invalid_message] = @invalid_message = check_word(DATA)
   end
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,13 @@
 module PagesHelper
+  def score(data)
+    time_taken = data[:time_end] - data[:time_start]
+    data[:word].length / time_taken
+  end
+
+  def check_word(data)
+    invalid_letters = data[:word].split('').reject do |letter|
+      data[:letters].split('').include? letter
+    end
+    return 'Invalid letters used!' unless invalid_letters.length.zero?
+  end
 end
